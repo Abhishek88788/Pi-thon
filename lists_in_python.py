22d6e246a0d8c1cdff6fdbb09ac3a5fb256f374f
ABHISHEK <116311711+Abhishek88788@users.noreply.github.com>
2025-11-09 12:52:06 +0000

    feat: add lists_in_python.py with examples of lists and string slicing

---
+++ lists_in_python.py
@@ -0,0 +1,33 @@
+marks = [45.4,545.3,234.3,433]
+print(marks)
+print(type(marks))
+
+
+#indexing 
+#same like array : we will access the elements of lists using index value
+
+# print(marks[3])
+# print(len(marks))
+
+# a = marks
+# print(a)
+
+
+#1. differece bitween  C ,C++ array and python's list( advance array)
+
+#list can store diferent types of data (int,str,float,bool,None)
+list = ["abhi",18,47.5,True,None]
+
+
+
+#2. difference bitween string and list
+ 
+#string is imutable (not change after initialize)
+#lists are mutable (can be change )
+
+#list slicing
+
+name = "Abhishek"
+
+print(name[-1:-9:-1])
+
