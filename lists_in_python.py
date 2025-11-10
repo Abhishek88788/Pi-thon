6ead0672edec63e3581a09cfee9dcf9e346f299d
ABHISHEK <116311711+Abhishek88788@users.noreply.github.com>
2025-11-10 15:35:25 +0000

    feat: initialize list in list_practiice2.py and expand examples in lists_in_python.py with insert method explanations

--- lists_in_python.py
+++ lists_in_python.py
@@ -42,3 +42,32 @@ fruits.remove("banana")  # Removing an element from the list
 print(fruits)
 fruits.sort()  # Sorting the list
 print(fruits)
+
+# insert method
+
+insert_list = [45,23,67,12]
+insert_list.insert(2, 99)  # Insert 99 at index 2 (intex starts from 0)
+print(insert_list)
+
+# if we inser at index greater than length of list then it will insert at the end of list
+insert_list.insert(13, 100)  # Insert 100 at index 10 (which is greater than length of list)
+print(insert_list)
+
+#answer is [45, 23, 99, 67, 12, 100]
+#why- because when we try to insert at an index greater than the current length of the list,
+# Python automatically appends the element to the end of the list.
+# This behavior ensures that the list remains contiguous and avoids creating gaps or undefined elements.
+# Thus, the element 100 is added at the end of the list.
+
+
+
+
+
+
+
+
+# # pop method
+# popped_element = insert_list.pop(3)  # Remove and return element at index 3
+# print("Popped element:", popped_element)
+# print("List after pop:", insert_lisat)
+
